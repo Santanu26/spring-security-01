9e068febfc83d716561da6efd69a589eeb21975e
Shantanu <santanu@jotno.net>
2020-09-08 11:43:38 +0600

    First commit for sprnig security revise 01 without spring security

---
+++ src/main/java/com/shantanu/ssecurity01/SSecurity01Application.java
@@ -0,0 +1,12 @@
+package com.shantanu.ssecurity01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSecurity01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSecurity01Application.class, args);
+    }
+}
---
+++ src/test/java/com/shantanu/ssecurity01/SSecurity01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantanu.ssecurity01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSecurity01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
